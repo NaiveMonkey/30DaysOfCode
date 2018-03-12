5c4e0b3a84b170f1ffc0addf53bdeeee306ce3f3
Saemiru Kim <ksmr1102@gmail.com>
2018-03-12 17:46:59 +0900

    Day 27 - solve problem

---
+++ src/Day27/Solution.java
@@ -0,0 +1,40 @@
+package Day27;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+//        Scanner sc = new Scanner(System.in);
+//        int testCase = sc.nextInt();
+//        for (int i = 0; i < testCase; i++){
+//            int attendNum = sc.nextInt();
+//            int atLeastNum = sc.nextInt();
+//            if (atLeastNum > attendNum) System.out.println("YES");
+//            int count = 0;
+//            for (int j = 0; j < attendNum; j++){
+//                int time = sc.nextInt();
+//                if (time <= 0) count++;
+//            }
+//            if (count >= atLeastNum) System.out.println("NO");
+//            else System.out.println("YES");
+//        }
+        System.out.println("5");
+
+        System.out.println("4 4");
+        System.out.println("-1 0 3 3");
+
+        System.out.println("6 2");
+        System.out.println("-1 0 1 2 3 2");
+
+        System.out.println("7 4");
+        System.out.println("-1 -2 0 3 3 9 6");
+
+        System.out.println("3 2");
+        System.out.println("-2 0 1");
+
+        System.out.println("8 5");
+        System.out.println("-2 -1 0 2 3 5 6 99");
+    }
+}
